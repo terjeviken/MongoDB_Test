672bc4202776fc457714ab3b2eef069a4b71d528
terjeviken <terje.viken2@outlook.com>
2018-02-21 18:15:40 +0100

    Implemented env variable MONGO_URI for mLab mongo db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 const {ObjectID} = require("mongodb");
-const db_host = "46.101.205.213";
+const db_host = "46.101.205.213"; // limited by firewall to dev pc
 const db_port = 27017;
 
+
+const  devUri = `mongodb://${db_host}:${db_port}/TodoApp`;
+const prodUri = process.env.MONGO_URI;
+
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://${db_host}:${db_port}/TodoApp`);
+mongoose.connect(process.env.PORT ? prodUri : devUri);
 
 
 var validID = function(the_id){
